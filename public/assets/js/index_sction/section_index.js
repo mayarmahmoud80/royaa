fff768cdafd8c935080ae00bc3cb091a25f0c5f3
mayarmahmoud80 <mahmoudmayar699@yahoo.com>
2025-12-02 14:47:22 +0200

    update

--- public/assets/js/index_sction/section_index.js
+++ public/assets/js/index_sction/section_index.js
@@ -23,10 +23,10 @@ if(element){
 
  
 const services = [
-  { title:"Branding", video:"https://res.cloudinary.com/doizn3h1h/video/upload/v1759712576/18419658-hd_1920_1080_30fps_ajaqks.mp4",  list:["Creative Direction","Brand Identity","Branding Strategy","Graphic Design","Startup"]},
-  { title:"UI-UX Design", video:"https://res.cloudinary.com/doizn3h1h/video/upload/v1759712548/12920659-hd_1920_1080_30fps_xg9jpi.mp4",list:["UI UX Consulting","UX Research","Usability Testing","Wireframing","Prototyping"]},
-  { title:"Development", video:"https://res.cloudinary.com/doizn3h1h/video/upload/v1759712578/8303104-hd_1920_1080_24fps_y87nso.mp4", list:["WordPress","Webflow","Laravel Framework","React & Flutter","Design System"]},
-  { title:"Digital Marketing", video:"https://res.cloudinary.com/doizn3h1h/video/upload/v1759713619/VIDEO_1_WEBSITE22_gjmcv7.mp4", list:["Online Marketing","SEO-Marketing","Strategy","Market Research","Social Reform"]}
+  { title:"Branding", video:"https://res.cloudinary.com/dampwi1tt/video/upload/v1763892154/VIDEO_1_WEBSITE22_evjkj7.mp4",  list:["Creative Direction","Brand Identity","Branding Strategy","Graphic Design","Startup"]},
+  { title:"UI-UX Design", video:"https://res.cloudinary.com/dampwi1tt/video/upload/v1763892052/12920659-hd_1920_1080_30fps_ngo96z.mp4",list:["UI UX Consulting","UX Research","Usability Testing","Wireframing","Prototyping"]},
+  { title:"Development", video:"https://res.cloudinary.com/dampwi1tt/video/upload/v1763892035/8303104-hd_1920_1080_24fps_gz95pv.mp4", list:["WordPress","Webflow","Laravel Framework","React & Flutter","Design System"]},
+  { title:"Digital Marketing", video:"https://res.cloudinary.com/dampwi1tt/video/upload/v1763891995/18419658-hd_1920_1080_30fps_arus6m.mp4", list:["Online Marketing","SEO-Marketing","Strategy","Market Research","Social Reform"]}
 ];
 
 function renderServices(containerId){
